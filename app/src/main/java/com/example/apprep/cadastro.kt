c288299250a568986787708fa307a9069b3db955
Hideki <sergionisikava@gmail.com>
2019-10-21 09:36:28 -0300

    Adicionado Botão de Login do Facebook. (link = https://developers.facebook.com/docs/facebook-login/android?sdk=maven)
    Adicionado tela de cadastro.

---
+++ app/src/main/java/com/example/apprep/cadastro.kt
@@ -0,0 +1,62 @@
+package com.example.apprep
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro.*
+
+class cadastro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        buttonFinalizarCadastro.setOnClickListener {
+            if(editTextNome.text.toString().isEmpty()){
+                editTextNome.requestFocus()
+                editTextNome.setError("Campo obrigatorio")
+                return@setOnClickListener
+            }
+
+            if(editTextSobrenome.text.toString().isEmpty()){
+                editTextSobrenome.requestFocus()
+                editTextSobrenome.setError("Campo obrigatorio")
+                return@setOnClickListener
+            }
+
+            if(editTextTelefone.text.toString().isEmpty()){
+                editTextTelefone.requestFocus()
+                editTextTelefone.setError("Campo obrigatorio")
+                return@setOnClickListener
+            }
+
+            if(editTextCPF.text.toString().isEmpty()){
+                editTextCPF.requestFocus()
+                editTextCPF.setError("Campo obrigatorio")
+                return@setOnClickListener
+            }
+
+            if(editTextEmail.text.toString().isEmpty()){
+                editTextEmail.requestFocus()
+                editTextEmail.setError("Campo obrigatorio")
+                return@setOnClickListener
+            }
+
+            if(editTextSenha.text.toString().isEmpty()){
+                editTextSenha.requestFocus()
+                editTextSenha.setError("Campo obrigatorio")
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, perfil::class.java)
+            startActivity(intent)
+        }
+
+        buttonVoltarCadastro.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/apprep/login.kt
+++ app/src/main/java/com/example/apprep/login.kt
@@ -1,12 +1,36 @@
 package com.example.apprep
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        printKeyHash()
+    }
+
+    private fun printKeyHash(){
+        try{
+            val info = packageManager.getPackageInfo("com.example.apprep", PackageManager.GET_SIGNATURES)
+            for(signature in info.signatures){
+                val md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray())
+                Log.e("KEYHASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        }
+        catch (e:PackageManager.NameNotFoundException){
+
+        }
+        catch (e:NoSuchAlgorithmException){
+
+        }
     }
 }
